67a1e7d1b55362e93d5bd1df284c5854448ef6a5
Alexander Syvitski <129970174+Alex-Syvitski@users.noreply.github.com>
2023-10-04 01:37:38 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,15 +4,18 @@ const coinImage = document.getElementById('coinImage');
 const coinSound = new Audio('https://raw.githubusercontent.com/Alex-Syvitski/CoinFlip/main/mouse-click.mp3');
 
 function flipCoin() {
-    if (Math.random() > 0.5) {
-        coinImage.src = 'https://raw.githubusercontent.com/Alex-Syvitski/CoinFlip/main/coin-boy.gif';
-    } else {
-        coinImage.src = 'https://raw.githubusercontent.com/Alex-Syvitski/CoinFlip/main/coin-girl.gif'; // replace with your tails image URL
-    }
-
-    // Play the sound
-    coinSound.play();
+    coinImage.classList.add('coin-flip-animation');
+    
+    setTimeout(() => {
+        if (Math.random() > 0.5) {
+            coinImage.src = 'https://raw.githubusercontent.com/Alex-Syvitski/CoinFlip/main/coin-boy.gif';
+        } else {
+            coinImage.src = 'https://raw.githubusercontent.com/Alex-Syvitski/CoinFlip/main/coin-girl.gif'; // replace with your tails image URL
+        }
+        coinImage.classList.remove('coin-flip-animation');
+    }, 500);
 }
+
 
 // Initial flip
 flipCoin();
